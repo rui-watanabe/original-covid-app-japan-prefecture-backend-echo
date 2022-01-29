7671b57cc8c1a73356858dbe24d61e6f5f3dd692
rui-watanabe <wataru610806@gmail.com>
2022-01-29 12:29:47 +0900

    set_log_#2

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"original-covid-app-japan-prefecture-backend-echo/config"
 
 	"github.com/labstack/echo"
 )
@@ -14,5 +15,5 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":" + config.Config.Port))
 }
