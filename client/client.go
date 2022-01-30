4ce8cb954c2794eafa7351a20a808ff2311108b3
rui-watanabe <wataru610806@gmail.com>
2022-01-30 13:49:26 +0900

    implement client #4　--date=Fri Jan 14 21:59:59 2022+0900

---
+++ client/client.go
@@ -0,0 +1,34 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+func RunClient() (p Prefectures) {
+	c, err := NewClient("https://opendata.corona.go.jp/api/covid19DailySurvey")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// http.Response として返却
+	res, err := c.Get(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = json.Unmarshal(body, p)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return
+}
